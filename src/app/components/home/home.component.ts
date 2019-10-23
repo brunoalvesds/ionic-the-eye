f0c97cc17e35fb972b4f5b8b17c22a58b392ed8f
G. Rodrigues da Silva <g.rodrigues.da.silva@avanade.com>
2019-10-23 11:41:29 -0300

    movendo logout para service

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,26 +3,21 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import { Router } from '@angular/router';
 import { ToastService } from 'src/app/services/toast.service';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router, private toast: ToastService) { }
+	constructor(private loginService: LoginService) { }
 
-  ngOnInit() {}
+	ngOnInit() { }
 
-  onLogout() {
-		firebase.auth().signOut()
-			.then(() => {
-				this.router.navigateByUrl('login');
-			})
-			.catch(error => {
-				this.toast.present(error.message);
-			});
+	onLogout() {
+		this.loginService.doLogout();
 	}
 
 }
