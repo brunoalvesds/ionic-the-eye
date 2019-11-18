a50c05ec1cdc2416e96bbde3c073323cda659f28
vitvbs <victorbaptista16@hotmail.com>
2019-11-18 13:02:20 -0300

    Tela de chamada

--- src/app/components/call/call.component.ts
+++ src/app/components/call/call.component.ts
@@ -1,14 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-call',
   templateUrl: './call.component.html',
   styleUrls: ['./call.component.scss'],
 })
-export class CallComponent implements OnInit {
+export class CallComponent{
 
-  constructor() { }
+  private date = new Date();
 
-  ngOnInit() {}
+  public formattedDate = this.date.toLocaleDateString()
+
+  public cards = [
+    {
+      'name' : 'Joao Silva',
+      'ra' : '81712259',
+      'image' : 'https://png.pngtree.com/svg/20161205/person_25302.png'
+    },
+    {
+      'name' : 'Maria Aparecida',
+      'ra' : '81712259',
+      'image' : 'https://png.pngtree.com/svg/20161205/person_25302.png'
+    },
+    {
+      'name' : 'Paulo Velozo',
+      'ra' : '81712259',
+      'image' : 'https://png.pngtree.com/svg/20161205/person_25302.png'
+    },
+    {
+      'name' : 'Aron Bado',
+      'ra' : '81712259',
+      'image' : 'https://png.pngtree.com/svg/20161205/person_25302.png'
+    },
+    {
+      'name' : 'Simas Turbando',
+      'ra' : '81712259',
+      'image' : 'https://png.pngtree.com/svg/20161205/person_25302.png'
+    },
+  ]
+
+  public present = [];
+
+  constructor(private navCtrl: NavController) { }
+
+  public removeBy(index: number): void {
+    this.cards.splice(index, 1);
+  }
+
+  public direction(event, index) {
+
+    if (event) {
+      this.present.push(this.cards[index])
+      console.log(this.present)
+    }
+
+  }
+
+  public goBack() {
+    this.navCtrl.back();
+  }
 
 }
